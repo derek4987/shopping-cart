1950806038d4a47b895e6d05046cd722d3c99612
Derek Nunes <dereknunes28@gmail.com>
2023-08-02 21:22:53 -0700

    Add Homepage content and css

--- src/routes/Homepage.js
+++ src/routes/Homepage.js
@@ -1,6 +1,7 @@
 // Homepage route page
 
 import React, { useState } from "react";
+import { Link } from "react-router-dom"
 import NavBar from "../components/NavBar";
 import '../components/css/Homepage.css';
 
@@ -8,7 +9,20 @@ const Homepage = () => {
     return (
       <div className="content" id="HomepageContent">
         <NavBar />
-        <h1>Hello from Homepage</h1>
+        <div id="hpContent">
+          <div className="hpTxt">
+            <div className="hpMainTxt">
+              <h1>Savings Up To</h1>
+              <h1>40% Off</h1>
+            </div>
+            <div className="hpSubTxt">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras consectetur consectetur elit, id lobortis.
+            </div>
+            <button className="hpBtn">
+              <Link to="/shop">Shop Now</Link>
+            </button>
+          </div>
+        </div>        
       </div>
     );
   };
