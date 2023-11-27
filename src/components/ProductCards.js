1ec09d7978aef72800e4a53589071a6473c5a4cf
derek4987 <98070075+derek4987@users.noreply.github.com>
2023-11-26 21:36:55 -0800

    initial ShopPage component and css setup

--- src/components/ProductCards.js
+++ src/components/ProductCards.js
@@ -1,1 +1,14 @@
 // product cards component
+
+import React, { useState } from "react";
+import './css/ProductCards.css';
+
+const ProductCards = (props) => {
+    return (
+        <div className="typCardLayout">
+
+        </div>
+    );
+}
+
+export default ProductCards;
--- src/routes/ShopPage.js
+++ src/routes/ShopPage.js
@@ -3,12 +3,14 @@
 import React, { useState } from "react";
 import NavBar from "../components/NavBar";
 import '../components/css/ShopPage.css';
+import ProductCards from "../components/ProductCards";
 
 const ShopPage = () => {
     return (
       <div className="content" id="ShopPageContent">
         <NavBar />
-        <h1>Hello from Shop Page</h1>
+        <h1 id="shopPageSpacer">Fake Store Shop</h1>
+        <ProductCards />
       </div>
     );
   };
