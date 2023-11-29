6808301da3537a0ddf17a09abe039bd4720546ef
derek4987 <98070075+derek4987@users.noreply.github.com>
2023-11-28 21:55:27 -0800

    ProductCard content layout

--- src/components/ProductCards.js
+++ src/components/ProductCards.js
@@ -4,9 +4,23 @@ import React, { useState } from "react";
 import './css/ProductCards.css';
 
 const ProductCards = (props) => {
+
+    const pcItemName = props.pcItemName;
+    const pcItemPhoto = props.pcItemPhoto;
+    const pcItemPrice = props.pcItemPrice;
+
     return (
         <div className="typCardLayout">
-
+            <div className="pcItemName">Test Item Name{pcItemName}</div>
+            <div className="pcItemPhotoDiv">
+                <img className="pcItemPhoto" src={pcItemPhoto}/>
+            </div>
+            <div className="pcItemBottomRibbon">
+                <div className="pcItemPrice">$10.00{pcItemPrice}</div>
+                <div>
+                    <button type="button" className="pcAddCartBtn">Add to Cart</button>    
+                </div>                
+            </div>
         </div>
     );
 }
